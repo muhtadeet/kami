627e7e5e9d8dede76378ab1a09d272dd55563c26
Muhtadee Taron <sheikh.taron@northsouth.edu>
2024-02-29 03:34:09 +0600

    ui fix

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -135,7 +135,7 @@ const Navigation = () => {
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar h-full bg-slate-800/70 overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          "group/sidebar h-full bg-primary/5 dark:bg-slate-800/70 overflow-y-auto relative flex w-60 flex-col z-[99999]",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0"
         )}
