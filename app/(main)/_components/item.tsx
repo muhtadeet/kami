90907de98ede224a1dfe103efd38d014a6c1a92b
Muhtadee Taron <sheikh.taron@northsouth.edu>
2024-02-29 16:33:10 +0600

    bug fixes

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -122,7 +122,7 @@ export const Item = ({
 
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+        <kbd className="ml-auto pointer-events-none hidden sm:inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs flex flex-row">
             ⌘<span className="text-sm hidden sm:block -mt-[0.1rem]">&#11212;K</span>
             <Sparkle className="sm:hidden h-3 w-3" />
