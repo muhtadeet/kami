97578f3f6e6edc6e97101da13660c38a64bdd41d
Muhtadee Taron <sheikh.taron@northsouth.edu>
2024-03-01 14:53:38 +0600

    Update layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,5 @@
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-full dark:bg-slate-900">{children}</div>;
+  return <div className="h-full flex dark:bg-[#1F1F1F]">{children}</div>;
 };
 
 export default PublicLayout;
