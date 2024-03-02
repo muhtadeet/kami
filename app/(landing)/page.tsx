a54d9b30f4c33c3ecf0e7ae33faa5b35d95ab997
muhtadeet <sheikh.taron@northsouth.edu>
2024-03-03 00:59:23 +0600

    login bug fixed

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import React from "react";
 import Heading from "./_components/heading";
-// import Heroes from "./_components/heroes";
-import Footer from "./_components/footer";
+import { useRouter } from "next/navigation";
+import { useConvexAuth } from "convex/react";
 
 const LandingPage = () => {
+  const router = useRouter();
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isAuthenticated) {
+    router.push("/documents");
+  }
+
   return (
     <div className="min-h-full flex flex-col overflow-hidden">
       <div className="flex flex-col items-center justify-center md:justify-normal text-center gap-y-8 flex-1 px-6 pb-10">
