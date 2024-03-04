ced900242077275784af3bec4bf1a7bb0d74f2ae
muhtadeet <sheikh.taron@northsouth.edu>
2024-03-04 17:24:35 +0600

    Update settings.tsx

--- components/modals/settings.tsx
+++ components/modals/settings.tsx
@@ -18,7 +18,7 @@ export const Settings = () => {
       </div>
       <div className="flex justify-center items-center mt-2 p-8 pt-3">
         <Link
-          href="https://github.com/muhtadeet/kami/releases/download/Kami/Kami-v1.3.apk"
+          href="https://github.com/muhtadeet/kami/releases/download/Kami/Kami-The.ultimate.note.app-v1.4.apk"
           target="_blank"
         >
           <button className="inline-flex h-7 animate-shimmer items-center justify-center rounded-full bg-[linear-gradient(110deg,#000103,45%,#3C4C63,55%,#000103)] bg-[length:200%_100%] px-4 py-6 font-medium text-white transition-all focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 transform ease-in-out duration-400">
