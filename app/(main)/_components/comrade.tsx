5f5b33f6049eacdd81941c65946d2cb36fa9a1c3
muhtadeet <sheikh.taron@northsouth.edu>
2024-03-04 16:52:47 +0600

    bug fixes

--- app/(main)/_components/comrade.tsx
+++ app/(main)/_components/comrade.tsx
@@ -7,7 +7,7 @@ const Comrade = () => {
     <div className="-ml-2">
       <Button size="sm" variant="ghost">
         <a href="https://comrade-flame.vercel.app/" target="_blank" className="flex flex-row">
-          <p className=" inline-flex h-7 animate-shimmer text-lg items-center justify-center rounded-full bg-gradient-to-r from-indigo-400 from-15% to-red-400 to-90% bg-[length:200%_100%] px-1 font-bold text-transparent transition-all focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 transform ease-in-out duration-400 bg-clip-text">
+          <p className=" inline-flex h-7 animate-shimmer2 text-lg items-center justify-center rounded-full bg-gradient-to-r from-indigo-400 from-15% to-red-400 to-90% bg-[length:200%_100%] px-1 font-bold text-transparent transition-all focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 transform ease-in-out duration-400 bg-clip-text">
             Ask
           </p>
           <Image
